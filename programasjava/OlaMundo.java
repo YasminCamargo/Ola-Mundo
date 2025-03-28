a4d6abb045515eb796876904bad9aaf69b3a52d2
YasminCamargo <Camargooyasmin16@gmail.com>
2025-03-28 15:51:51 -0300

    Hora-do-sistema
    Programa que diz a hora que está no sistema

--- programasjava/OlaMundo.java
+++ programasjava/OlaMundo.java
@@ -1,6 +1,6 @@
 package programasjava;
 public class OlaMundo {
     public static void main(String[] args) {
-        System.out.print("Olá, Mundo!");
+        System.out.println("Olá, Mundo!");
     }
 }
