5c90c2323f25385a9bb1e2069200eecb181fb679
YasminCamargo <Camargooyasmin16@gmail.com>
2025-04-01 15:47:27 -0300

    Soma entre 2 valores
    Programa que soma 2 valores ja estabelecidos

---
+++ programasjava/Soma.java
@@ -0,0 +1,12 @@
+package programasjava;
+
+public class Soma {
+    public static void main(String[] args) {
+        int n1 = (int) 2;
+        int n2 = (int) 4;
+        int soma = (int) n1 + (int) n2;
+
+        System.out.println("A soma entre " + n1 + " e " + n2 + " é " + soma);
+    }
+    
+}
