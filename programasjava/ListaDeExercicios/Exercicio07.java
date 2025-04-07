dea9c40b7f900869c6da3a739516715cf369dbc4
YasminCamargo <Camargooyasmin16@gmail.com>
2025-04-07 15:44:52 -0300

    Exercicio 07: Maior número digitado

---
+++ programasjava/ListaDeExercicios/Exercicio07.java
@@ -0,0 +1,11 @@
+package programasjava.ListaDeExercicios;
+
+import java.util.Scanner;
+
+public class Exercicio07 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+        for 
+    }
+}
