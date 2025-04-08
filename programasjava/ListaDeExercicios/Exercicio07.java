e4f66f7880e7c55fdd007501b6f5dcbfd7880210
YasminCamargo <Camargooyasmin16@gmail.com>
2025-04-08 16:19:47 -0300

    Exercicio 07: Maior número
     Programa que mostra o maior número digitado.

--- programasjava/ListaDeExercicios/Exercicio07.java
+++ programasjava/ListaDeExercicios/Exercicio07.java
@@ -5,7 +5,16 @@ import java.util.Scanner;
 public class Exercicio07 {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
+        int maior = 0;
 
-        for 
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Digite um número: ");
+            int numero = teclado.nextInt();
+
+            if (numero > maior) {
+                maior = numero;
+            }
+        }
+        System.out.println("O maior número digitado foi: " + maior);
     }
 }
