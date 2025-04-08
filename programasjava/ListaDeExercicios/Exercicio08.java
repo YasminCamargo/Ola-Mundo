ec7f865afb1c50ecd2eb313b65aa934ed2f04a69
YasminCamargo <Camargooyasmin16@gmail.com>
2025-04-08 16:34:40 -0300

    Exercicio 08: Soma 50 números
    Soma os 50 primeiros números

---
+++ programasjava/ListaDeExercicios/Exercicio08.java
@@ -0,0 +1,12 @@
+package programasjava.ListaDeExercicios;
+
+public class Exercicio08 {
+    public static void main(String[] args) {
+        int soma = 0;
+
+        for (int i = 0; i <= 50; i++) {
+            soma += i;
+        }
+        System.out.println("A soma dosprimeiros 50 números inteiros é: " + soma);
+    }
+}
