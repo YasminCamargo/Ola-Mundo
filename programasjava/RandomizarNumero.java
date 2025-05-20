a4ddd95fed3040c10aa4e70d6c9ece9c30b6c355
YasminCamargo <Camargooyasmin16@gmail.com>
2025-05-20 15:48:47 -0300

    Update RandomizarNumero.java

--- programasjava/RandomizarNumero.java
+++ programasjava/RandomizarNumero.java
@@ -10,6 +10,9 @@ public class RandomizarNumero {
 
         System.out.println("Número aleatório: " + numRandom.nextInt(20)); //Gera um número aleatório POSITIVO entre 0 e 19 (exclui o 20)
 
-        System.out.println("Número aleatório: " + (numRandom.nextInt(20) + 1));
+        System.out.println("Número aleatório: " + (numRandom.nextInt(20) + 1)); //Gera um número aleatório positivo entre 1 e 20 (inclui o 20)
+
+        System.out.println("Número aleatório: " + (numRandom.nextInt(21) + 5)); //Gera um número entre 5 e 25 (O número limite superior será 25-5+1=31 e 5 precisa ser
+                                                                                      //somado ao resultado)
     }
 }
