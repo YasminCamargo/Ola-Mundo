bab9a93cc74898030b7784035f9140fe4419d729
Yasmin Camargo <Camargooyasmin16@gmail.com>
2025-07-24 18:10:27 -0300

    Update

---
+++ programas-python/desafio-08-python.py
@@ -0,0 +1,16 @@
+frutas = ["Maçã", "Manga", "Uva", "Banana"]
+
+#Mudando itens e acrescentando itens no final
+frutas[1] = "Morango"
+frutas.append("Abacaxi")
+
+print(frutas)
+print("////////////////////////")
+
+#Mudando dois itens ao mesmo tempo (só vai mudar o index 1 e 2, o 3 não conta)
+frutas[1:3] = ["Abacaxi", "Abacate"]
+print(frutas)
+
+#Substitui somente um index (1, não conta o 2) por 2 itens
+frutas[1:2] = ["Goiaba", "Pêra"]
+print(frutas)
