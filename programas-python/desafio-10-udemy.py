1e0895a933bfb186656113f75747493891015e06
Yasmin Camargo <Camargooyasmin16@gmail.com>
2025-07-29 14:18:05 -0300

    Mais desafios

---
+++ programas-python/desafio-10-udemy.py
@@ -0,0 +1,4 @@
+frutas = ["Maçã", "Morango", "Uva"]
+
+for fruta in frutas:
+ print("Eu gosto de", fruta)
---
+++ programas-python/desafio-12-udemy.py
@@ -0,0 +1,6 @@
+frutas = ["Maçã", "Banana", "Uva"]
+vegetais = ["Pepino", "Brócolis", "Cenoura"]
+
+for fruta in frutas:
+    for vegetal in vegetais:
+        print(fruta, vegetal)
